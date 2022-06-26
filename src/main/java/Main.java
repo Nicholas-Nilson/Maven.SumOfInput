ca9b84a1519f2301d7f1eb11410d316f827fa19a
Nick Nilson <nilson.nicholas@gmail.com>
2022-06-25 22:25:14 -0400

    update while attempting to convert summing method to its own class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+//the program is working as intended, but I would like to reprogram it so the summing method is in another class. It is something I need to practice.
 public class Main {
 
     public static void main(String[] args){
+
+        int incrementedNumber = 1;
+        int sumTotal = 0;
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a whole number");
+        int userNumber = userInput.nextInt();
+        10
+
+        while (incrementedNumber <= userNumber) {
+            sumTotal = sumTotal + incrementedNumber;
+            incrementedNumber++;
+        }
+        System.out.println("Result is " + sumTotal);
 
     }
 }
---
+++ src/main/java/numSummer.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class numSummer {
+
+//    int incrementedNumber = 1;
+//    int sumTotal = 0;
+//    int userNumber;
+
+    public static int summing (int incrementedNumber, int sumTotal, int userNumber) {
+
+
+    }
+//    int incrementedNumber = 1;
+//    int sumTotal = 0;
+//    Scanner userInput = new Scanner(System.in);
+//        System.out.println("Please enter a whole number");
+//    int userNumber = userInput.nextInt();
+//        10
+//
+//                while (incrementedNumber <= userNumber) {
+//        sumTotal = sumTotal + incrementedNumber;
+//        incrementedNumber++;
+//    }
+//        System.out.println("Result is " + sumTotal);
+}
